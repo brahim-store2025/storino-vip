c07273602a4414cfbd3506ec065c794e16b46378
brahim-store2025 <oulbabrahim2025@gmail.com>
2025-05-23 01:44:43 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// عداد الزوار
+
 let visitors = 1258;
 setInterval(() => {
-    visitors += Math.floor(Math.random() * 3);
+    visitors += Math.floor(Math.random() * 5);
     document.getElementById('visitors').innerText = visitors.toLocaleString();
 }, 2000);
 
-// توقيت المغرب
 function updateMoroccoTime() {
     const options = { 
         timeZone: 'Africa/Casablanca',
         hour: '2-digit', 
         minute: '2-digit',
-        hour12: true 
+        second: '2-digit',
+        hour12: false 
     };
     const time = new Date().toLocaleTimeString('ar-MA', options);
     document.getElementById('morocco-time').innerText = time;
